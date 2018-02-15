94c761e4447968a86b6e0df5a15192b1c14a59bf
bspatter <i.am.brandon.patterson@gmail.com>
2018-02-15 11:11:08 -0500

    Changed histology_lab_colors.m to include the color of hemorrhage if given the input "hem". Altered process_microscope_images.m to segment image using this color as well. Might be worth seeing if adding color messes with images without hemorrhage. It might not since the necessary colors should already be included.

--- histology_lab_colors.m
+++ histology_lab_colors.m
@@ -1,10 +1,10 @@
-function [hist_lab_colors, hist_lab_colors_labels] = histology_lab_colors()
+function [hist_lab_colors, hist_lab_colors_labels] = histology_lab_colors(varargin)
 
 %
 % Syntax:  [output] = function_name(inputs)
 %
 % Inputs: NONE
-%   1. 
+%   1. 'hem' (Optional) input that includes the hemorrhage red color
 %
 % Outputs:
 %	1. 
@@ -27,5 +27,16 @@ function [hist_lab_colors, hist_lab_colors_labels] = histology_lab_colors()
 %
 % Last revision: 02-13-2018
 
-hist_lab_colors = [0.5267, 0.6138; 10.5938,  -18.7697; 13.4934, -4.1837; 3.5312, -0.7602]; % A and B values for colors in lung histology, based on LAB color space.
-hist_lab_colors_labels = {'white','dark_purple','light_purple','transparent_purple'};
+hist_lab_colors = [0.5267, 0.6138; 10.5938,  -18.7697; 13.4934, -4.1837; 3.5312, -0.7602; 32.2967, 6.2946]; % A and B values for colors in lung histology, based on LAB color space.
+hist_lab_colors_labels = {'white','dark_purple','light_purple','transparent_purple', 'red'};
+
+%
+if nargin==0
+    hist_lab_colors = hist_lab_colors(1:end-1,:);
+    hist_lab_colors_labels = hist_lab_colors_labels(1:end-1);
+elseif contains(lower(varargin{1}),'hem')
+    
+else
+    error('Bad input to histology_lab_colors. Current accepted inputs : ''hem''') 
+end
+        
