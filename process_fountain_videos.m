137a40cf86eb0c62e0eba690800f9cac29af17ab
bspatter <i.am.brandon.patterson@gmail.com>
2018-04-26 10:56:50 -0400

    proccess_fountain_videos.m now reads videos frame by frame and tries to identify which pixels correspond to fountains and drops based on pixel intensity levels from each  binarized frame, relative to an original frame w/o the fountain.

--- process_fountain_videos.m
+++ process_fountain_videos.m
@@ -34,15 +34,36 @@ strip_extension = @(mystr) mystr(1:(find(mystr=='.',1,'last')-1));
 % 2018-04-16 : Fountain images taken on 2018-04-05
 % based on matlab tutorial for motion-based multiple object tracking for computer vision toolbox
 
-CaseCode = 'Da';
+CaseCode = 'Db';
 basedir = 'E:/brandon/research/acoustic_fountain/acoustic_fountain_videos/';
 
 if true
     fdir=sprintf('%s%s/',basedir,CaseCode);
-    fnames = ls([fdir '*.avi']);
-    fname = fnames(1,:);
-    fpath = [fdir,fname];
+    fnames = ls([fdir '*.avi']); %All video file names in the relevant directory
+    fname = fnames(1,:); %Specific video file name
+    fpath = [fdir,fname]; % Path to video file
     
+    v =VideoReader(fpath);
+    v.CurrentTime = 5.0; % just for debugging for now;
+    
+    frame = readFrame(v);    
+    frame0 = frame;
+    
+    frame_index = {89:size(frame0,1), 129:640};
+
+    vidframe0=frame0(frame_index{:}); %extract only the video (lose the border)  %
+    vidframe = vidframe0;
+
+    while hasFrame(v)
+        frame = readFrame(v);
+        vidframe = frame(frame_index{:});
+        diffimage = imsubtract(vidframe,vidframe0);
+%         framep = vidframe;
+%         framep(diffimage==0)=0;
+%         imshow(diffimage)
+        imshowpair(vidframe0,vidframe)
+        
+    end
     
     
 end
